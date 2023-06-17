2feee7dd41955bbb7eb117840bf47468d5d7101b
Jaewon <tjrwodnjs99@gmail.com>
2023-06-17 17:26:36 +0900

    Fix

--- seminar/settings.py
+++ seminar/settings.py
@@ -42,13 +42,13 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
